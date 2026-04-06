801ebaca5c9079f735b98191fef8e75fa356e9aa
faizan4dev <faizan4dev@gmail.com>
2026-04-06 06:58:06 +0500

    first commit

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+list = [20,10,40,30,50]
+def insertion_sort(arr):
+    for j in range(1,len(arr)):
+        key = arr[j]
+        i=j-1
+
+        while i>=0 and arr[i] > key :
+            arr[i+1]=arr[i]
+            i=i-1
+        
+        arr[i+1]=key
+    print(arr)
+
+insertion_sort(list)
+
+    
